f91de597816cf762da47611506734c06be0ff4b6
gubatron <gubatron@gmail.com>
2017-02-05 22:00:48 -0700

    new StopWatchLoop thread controller

---
+++ app/src/main/java/com/kataysantos/movementstopwatch/StopWatchLoopController.java
@@ -0,0 +1,10 @@
+package com.kataysantos.movementstopwatch;
+
+/**
+ * Created by gubatron on 2/5/17.
+ */
+
+public interface StopWatchLoopController {
+    void startStopWatchLoop();
+    void stopStopWatchLoop();
+}
