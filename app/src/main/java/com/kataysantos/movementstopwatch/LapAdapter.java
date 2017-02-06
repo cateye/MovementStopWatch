faf56f0d8eec06102d5fc74c549fc475baec5c63
gubatron <gubatron@gmail.com>
2017-02-05 21:28:45 -0700

    cleanup

--- app/src/main/java/com/kataysantos/movementstopwatch/LapAdapter.java
+++ app/src/main/java/com/kataysantos/movementstopwatch/LapAdapter.java
@@ -8,23 +8,16 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.logging.Logger;
-
 /**
  * Created by katay on 2/2/17.
  */
 class LapAdapter extends ArrayAdapter<StopWatch.Time> {
-
-    private static Logger LOG = Logger.getLogger("LapAdapter");
     LapAdapter(Context context) {
         super(context, 0);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LOG.info("getView(position="+position+")");
-
         StopWatch.Time lap = getItem(position);
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.timelapsed_list_item, parent, false);
@@ -47,12 +40,10 @@ class LapAdapter extends ArrayAdapter<StopWatch.Time> {
 
     @Override
     public void registerDataSetObserver(DataSetObserver observer) {
-        observer.onChanged();
     }
 
     @Override
     public void unregisterDataSetObserver(DataSetObserver observer) {
-       LOG.info("unresigerDataSetObserver");
     }
 
     @Override
